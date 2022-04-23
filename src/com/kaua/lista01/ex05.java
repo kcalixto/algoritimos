8baf856dce00ee8198e6a933941b904f97fcc0a9
Kaua <kauacalixtocontato@gmail.com>
2022-04-23 18:31:24 -0300

    lista 02 - ex 1 - 14

--- src/com/kaua/lista01/ex05.java
+++ src/com/kaua/lista01/ex05.java
@@ -4,13 +4,13 @@ import java.util.Scanner;
 
 public class ex05 {
     public static void main(String[] args) {
-        System.err.println("ex05 - Fazer um algoritmo que leia um valor L e calcula a área de um quadrado de lado L.");
+        System.out.println("ex05 - Fazer um algoritmo que leia um valor L e calcula a área de um quadrado de lado L.");
 
         int l;
 
         Scanner input = new Scanner(System.in);
 
-        System.err.println("Digite o valor de L: ");
+        System.out.println("Digite o valor de L: ");
         l = input.nextInt();
 
         System.out.println("A área de um quadrado de lado " + l + " é: " + (l * l));
